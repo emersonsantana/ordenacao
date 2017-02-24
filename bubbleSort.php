6955144faa40c8f91b631c1142ba0b090c7353b8
Emerson Santana Cunha <ermim-nantes@hotmail.com>
2017-02-24 15:08:37 -0300

    Update bubbleSort.php

--- bubbleSort.php
+++ bubbleSort.php
@@ -3,33 +3,28 @@
  *  MÉTODO DE ORDENAÇÃO -- Bubble Sort -- em php array
  *  Por Emerson Santana Cunha
  */
-
 function bubbleSort($array)
 {
-	$TL2 = count($array);
-	
+	$TL2 = count($array);	
 	while($TL2 > 0) 
 	{
 		for($i=0; $i < $TL2-1; $i++)
 		{
 			if($array[$i+1] < $array[$i]) // próximo menor que atual no i
-			{
-				//realiza a troca, com uso de auxiliar
+			{   //realiza a troca, com uso de auxiliar
 				$aux = $array[$i+1];
 				$array[$i+1] = $array[$i];
 				$array[$i] = $aux;
 			} 
-
 		}
 		$TL2--; //decrementa o TL2, pois um elemento já esta na posição correta.
 	}
 return $array;
 }
-
 $vet = array(10,9,8,7,6,5,4,3,2,1);
-
 var_dump($vet);
-echo "<br/>";
+     echo "<br/>";
+//Passa o array para o método Bolha
 $vet = bubbleSort($vet);
-
+//Exibe o array ordenado
 var_dump($vet);
